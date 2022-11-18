99f0891a3bdd03efd218947cfaa599f539cc3fef
fernando-izar <fizar@me.com>
2022-11-18 14:49:50 -0300

    feat: add reset at global.js

--- src/styles/global.js
+++ src/styles/global.js
@@ -7,4 +7,25 @@ export const GlobalStyle = createGlobalStyle`
         --Color-primary-gradient: #102542
     }
 
+    
+* {
+    margin: 0;
+    box-sizing: border-box;
+}
+
+button {
+    cursor: pointer;
+}
+
+ul, ol, li {
+    list-style: none;
+    margin: 0px;
+    padding: 0px;
+
+}
+
+a, u {
+    text-decoration: none;
+}
+
 `;
