254e0acfa2ff71ac5a456395408b9ff7352f9147
Bruno Goniadis <brunogoniadislima@gmail.com>
2022-12-02 00:13:56 -0300

    styled: Logo, header, card , dash

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -13,28 +13,35 @@ export const Card = ({
 }: IProducts) => {
   return (
     <Container>
-      <div className="flip-card-inner">
-        <div className="flip-card-front">
-          <img src={image} alt={image} />
-          <div className="flip-card-front-title">
+
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <div className="imageContainer">
+              <img src={image} alt={image} />
+              <img src={image} alt={image} />
+              <div className="blurDiv">________________________</div> {/*&nbsp;*/}
+              <div className="imgReverse"> </div>
+            </div>
+            <div className="flip-card-front-title">
+              <div>
+                <p>{title}</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="flip-card-back">
+            <div className="flip-card-back-title">{title}</div>
+            <div className="flip-card-back-price">${price}</div>
+            {/* <div className="flip-card-back-description">{description}</div> */}
+            <div className="flip-card-back-rate">Rate: {rating.rate}</div>
+            <div className="flip-card-back-count">Count: {rating.count}</div>
+            <div>{description}</div>
             <div>
-              <p>{category}</p>
+              <Link to={`/dashboard/${id}`}>Description</Link>
             </div>
           </div>
         </div>
 
-        <div className="flip-card-back">
-          <div className="flip-card-back-title">{title}</div>
-          <div className="flip-card-back-price">${price}</div>
-          {/* <div className="flip-card-back-description">{description}</div> */}
-          <div className="flip-card-back-rate">Rate: {rating.rate}</div>
-          <div className="flip-card-back-count">Count: {rating.count}</div>
-          <div>{description}</div>
-          <div>
-            <Link to={`/dashboard/${id}`}>Description</Link>
-          </div>
-        </div>
-      </div>
     </Container>
   );
 };
