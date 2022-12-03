0886c31bdbb35a633f371c66a5e6467196ef6a31
fernando-izar <fizar@me.com>
2022-12-03 19:12:19 -0300

    feat: useEffect on UserContext, tracked by loading, created

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,4 +1,8 @@
-import { ReactNode, useState, createContext } from "react";
+import { ReactNode, useState, createContext, useEffect } from "react";
+import { IUser } from "../interfaces/user.interfaces";
+import { api } from "../services/api";
+import { useNavigate } from "react-router-dom";
+import { SubmitHandler } from "react-hook-form";
 
 interface IUserProviderProps {
   children: ReactNode;
@@ -14,7 +18,30 @@ export const UserContext = createContext<IUserProviderData>(
 );
 
 export const UserProvider = ({ children }: IUserProviderProps) => {
+  const [user, setUser] = useState<IUser | null>(null);
   const [loading, setLoading] = useState(true);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const LoadUser = async () => {
+      const token = localStorage.getItem("@TOKEN");
+
+      if (token) {
+        try {
+          const userId = localStorage.getItem("@USERID");
+          api.defaults.headers.common.authorization = `Bearer ${token}`;
+
+          const { data } = await api.get<IUser>(`users/${userId}`);
+          setUser(data);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      setLoading(false);
+    };
+    LoadUser();
+  }, [loading]);
 
   return (
     <UserContext.Provider value={{ loading, setLoading }}>
