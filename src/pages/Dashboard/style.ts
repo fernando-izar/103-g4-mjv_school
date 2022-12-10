a1efdf577950e3a8f55a93432c459a98b6838a54
fernando-izar <fizar@me.com>
2022-12-10 13:13:37 -0300

    fix: change width to 100vw at Container in the Dashboard to adjust the size to the screen

--- src/pages/Dashboard/style.ts
+++ src/pages/Dashboard/style.ts
@@ -3,18 +3,17 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   background-color: var(--color-secondary);
-  width: 200vh;
+  width: 100vw;
   height: 100vh;
   .main-content {
     width: 85%;
     height: 63vh;
-    .marketingContainer{
+    .marketingContainer {
       width: 99%;
     }
-  
   }
-  h2{
+  h2 {
     margin-left: 10px;
     margin-top: 5px;
-  }  
+  }
 `;
